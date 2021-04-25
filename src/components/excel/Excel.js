cc6b70e024676d1433c2c6de798f052752fe26c6
Slava <slava91petrushin@yandex.ru>
2021-04-25 21:14:40 +0300

    create dinamic table

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,34 +1,29 @@
-import { $ } from "../../core/dom";
+import {$} from "../../core/dom";
 
 export class Excel {
-	constructor(selector, options){
-		this.$el = $(selector);
-		this.components = options.components || [];
-	}
+    constructor(selector, options){
+        this.$el = $(selector);
+        this.components = options.components || [];
+    }
 
-	getRoot(){
-		let $root = $.create('div', 'excel');
-		this.components = this.components.map(Component => {
-			let $el = $.create('div', Component.className);
-			let component = new Component($el);
+    getRoot () {
+        let $root = $.create('div', 'excel')
+        this.components = this.components.map(Component => {
+            const $el = $.create('div', Component.className);
+            const component = new Component($el);
 
-		//DEBUD
-		if(component.name){
-			window['c' + component.name] = component
-		}
+            $el.html(component.toHTML());
+            $root.append($el);
 
-			$el.html(component.toHTML());
-			$root.append($el);
+            return component;
+        });
+        return $root;
+    }
 
-			return component;
-		});
-		return $root;
-	}
-
-	render(){
-		this.$el.append(this.getRoot());
-		this.components.forEach(Component => {
-			Component.init();
-		});
-	}
+    render(){
+        this.$el.append(this.getRoot());
+        this.components.forEach(Component => {
+            Component.init();
+        });
+    }
 }
---
+++ src/components/table/table.template.js
@@ -0,0 +1,50 @@
+const CODES = {
+	A: 65,
+	Z: 90
+};
+
+function createRow(value, info = ''){
+	return `
+		<div class="row">
+			<div class="row-info">${info}</div>
+			<div class="row-data">${value}</div>
+		</div>
+	`;
+}
+
+function createCell(value = ''){
+	return `<div class="cell">${value}</div>`
+}
+
+function createCol(col){
+	return `<div class="column">${col}</div>`;
+}
+
+function toChar(_,index){
+	return String.fromCharCode(CODES.A + index);
+}
+
+export function createTable(rowsCount = 15){
+	const rows = [];
+	const colsCount = CODES.Z - CODES.A + 1;
+	const cols = new  Array(colsCount)
+		.fill('')
+		.map(toChar)
+		.map(createCol)
+		.join('');
+		
+	const cells = new Array(colsCount)
+		.fill('')
+		.map(createCell)
+		.join('')
+
+	for (let i = 0; i < rowsCount; i++){
+		if(i === 0){
+			rows.push(createRow(cols));
+		} else {
+			rows.push(createRow(cells, i+1));
+		}	
+	}
+
+	return `${rows.join('')}`
+}
